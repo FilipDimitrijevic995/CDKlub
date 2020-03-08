e6cad440819fa5f316eecb5d99030d261835018c
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2020-03-08 11:58:33 +0100

    Implementing Validation

--- CDKlub/Controllers/CustomersController.cs
+++ CDKlub/Controllers/CustomersController.cs
@@ -28,6 +28,8 @@ namespace CDKlub.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
+
                 MembershipTypes = membershipTypes
             };
 
@@ -35,8 +37,20 @@ namespace CDKlub.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
