9553fac079dbcbc16f0d413b4b19858330fca1ce
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2020-03-09 18:14:29 +0100

    Add DataTables to movies list and implement delete movie.

--- CDKlub/Dtos/MovieDto.cs
+++ CDKlub/Dtos/MovieDto.cs
@@ -16,6 +16,8 @@ namespace CDKlub.Dtos
 
         [Required]
         public byte GenreId { get; set; }
+        
+        public GenreDto Genre { get; set; }
 
         public DateTime DateAdded { get; set; }
 
